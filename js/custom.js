84067e279ece2a2f76ddd0bc5f845fd2bcb9f0a3
Gokul <gokul.krishna@plash.in>
2017-12-01 20:17:32 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -2,19 +2,56 @@
 $(document).ready(function() {
     $("#dynamic_footer").load("footer.html");
     $("#header").load("header.html");
+
+    $("#emailLink").click(function()
+    {
+        window.open('mailto:contact@dtale.io','_blank','',true);
+    });
+    // $('body').click(function() {
+    //     console.log("outside",menuFlag);
+    //     if(menuFlag){
+    //         menuFlag=false;
+    //         console.log("inside if");
+    //     }else{
+    //         $('.sidenav').removeClass('width_100');
+    //         $('body').removeClass('has-mobile-navigation-open');
+    //         console.log("inside close");
+    //         menuFlag=true;
+    //     }
+    // });
+    // $('.menuclose ').click(function() {
+    //     $('.sidenav').removeClass('width_100');
+    //     $('body').removeClass('has-mobile-navigation-open');
+    //
+    // });
     return false;
 
 });
-var menuFlag;
+
+// var menuFlag=false;
 function toggleDemo(){
-    $('body').toggleClass('has-mobile-navigation-open');
-    $('.sidenav').toggleClass('width_100');
-     menuFlag=true;
+
+
+
+    // if(!menuFlag) {
+        $('body').toggleClass('has-mobile-navigation-open');
+        $('.sidenav').toggleClass('width_100');
+        // $('.mobile-nav-toggle-button').addClass('menuclose');
+        // menuFlag = true;
+    //
+    // }
+    // else{
+    //     $('body').removeClass('has-mobile-navigation-open');
+    //     $('.sidenav').removeClass('width_100');
+    // }
+    // console.log("inside on clice ");
 }
+
+
+
 window.onload = function() {
     setTimeout(function() {
         $(".loader").css("visibility", "hidden");
-
     }, 0);
 };
 
